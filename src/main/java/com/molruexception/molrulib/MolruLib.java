872dbe3e077dc732aca7fe7e02eb9c9fe16e983d
MolruException <molruexception@gmail.com>
2022-10-17 17:16:26 +0900

    Chore: 프로젝트 셋업

---
+++ src/main/java/com/molruexception/molrulib/MolruLib.java
@@ -0,0 +1,17 @@
+package com.molruexception.molrulib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MolruLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
